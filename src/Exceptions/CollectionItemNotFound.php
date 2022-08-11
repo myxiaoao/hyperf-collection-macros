0cff33ae4c0c4cfb70ccd4a71a03269fb4097c07
Cooper <myxiaoao@gmail.com>
2022-08-11 14:57:24 +0800

    update header

--- src/Exceptions/CollectionItemNotFound.php
+++ src/Exceptions/CollectionItemNotFound.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Hyperf.
+ * This file is part of cooper/hyperf-collection-macros.
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * @link     https://github.com/myxiaoao/hyperf-collection-macros
+ * @document https://github.com/myxiaoao/hyperf-collection-macros/blob/master/README.md
+ * @contact  myxiaoao@gmail.com
+ * @license  https://github.com/myxiaoao/hyperf-collection-macros/blob/master/LICENSE
  */
 namespace Cooper\HyperfCollectionMacros\Exceptions;
 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Hyperf.
+ * This file is part of cooper/hyperf-collection-macros.
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * @link     https://github.com/myxiaoao/hyperf-collection-macros
+ * @document https://github.com/myxiaoao/hyperf-collection-macros/blob/master/README.md
+ * @contact  myxiaoao@gmail.com
+ * @license  https://github.com/myxiaoao/hyperf-collection-macros/blob/master/LICENSE
  */
 require_once dirname(__FILE__, 2) . '/vendor/autoload.php';
